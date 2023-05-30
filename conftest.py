ce0496beaff9ba8e29783bf51c0aaf3475c3ad19
Iulia Klass <YuliiyaKlass@yandex.ru>
2023-05-30 22:20:57 +0200

    changes after review

--- conftest.py
+++ conftest.py
@@ -1,40 +1,22 @@
 import pytest
 from selenium import webdriver
-from faker import Faker
 from locators import Locators as L
-import time
-
-faker = Faker()
-
-@pytest.fixture
-def email():
-    return faker.email()
+from helper import ValidCredentials, Urls
 
 
-# параметризованная фикстура автозакрывающегося драйвера, тест запустится на обоих браузерах
-# https://roman.pt/posts/parametrizing-pytest-fixtures/
-@pytest.fixture(params=[webdriver.Chrome, webdriver.Firefox])
-def driver(request):
-    driver = request.param()
+@pytest.fixture()
+def driver():
+    driver = webdriver.Chrome()
     driver.maximize_window()
+    driver.get(Urls.MAIN)
     yield driver
     driver.quit()
 
 
 @pytest.fixture
-def valid_credentials():
-    return "HM@gmail.com", "123456"
-
-
-@pytest.fixture
-def logged_in_driver(driver, valid_credentials):
-    email, password = valid_credentials
-    driver.get("https://stellarburgers.nomoreparties.site")
-    time.sleep(2)
+def logged_in_driver(driver):
     driver.find_element(*L.ACCOUNT_BUTTON).click()
-    time.sleep(1)
-    driver.find_element(*L.LOGIN_EMAIL).send_keys(email)
-    driver.find_element(*L.PASSWORD_INPUT_NAME).send_keys(password)
-    time.sleep(1)
+    driver.find_element(*L.LOGIN_EMAIL).send_keys(ValidCredentials.email)
+    driver.find_element(*L.PASSWORD_INPUT_NAME).send_keys(ValidCredentials.password)
     driver.find_element(*L.LOGIN_BUTTON).click()
     return driver
---
+++ helper.py
@@ -0,0 +1,12 @@
+
+class ValidCredentials:
+    email = "HM@gmail.com"
+    password = "123456"
+
+
+class Urls:
+    MAIN = "https://stellarburgers.nomoreparties.site/"
+    ACCOUNT = "https://stellarburgers.nomoreparties.site/account"
+    PROFILE = "https://stellarburgers.nomoreparties.site/account/profile"
+    LOGIN = "https://stellarburgers.nomoreparties.site/login"
+    REGISTER = "https://stellarburgers.nomoreparties.site/register"
